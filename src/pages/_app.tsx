483f50e1f85f95a08277d24cd3a14fe9ca18ec74
Inbestigator <inbestigatoryt@gmail.com>
2024-04-20 20:20:44 -0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: any) {
       <Head>
         <title>Gneissname</title>
         <meta name="og:title" content="Gneissname" />
-        <meta property="og:image" content="cdn/icon.png" />
+        <meta property="og:image" content="/cdn/icon.png" />
         <meta name="theme-color" content="#020202" />
       </Head>
       <Component {...pageProps} />
