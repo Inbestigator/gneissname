7aac52d8f94da87383ecca8f2ad5a162d0bab076
Inbestigator <inbestigatoryt@gmail.com>
2024-05-04 22:09:42 -0700

    New vid

--- src/lib/fetchYT.ts
+++ src/lib/fetchYT.ts
@@ -6,7 +6,7 @@ export async function getLatestVideos(): Promise<Video[]> {
       {
         thumbnail:
           "https://i.ytimg.com/vi/6nulqmkLKqI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCY-iVXyn9jnEYFTvH2lPFcXAuADg",
-        index: 0,
+        index: 1,
         description:
           "Discussion on how geology affects people and what geologists do.",
         id: "6nulqmkLKqI",
@@ -15,19 +15,19 @@ export async function getLatestVideos(): Promise<Video[]> {
       {
         thumbnail:
           "https://i.ytimg.com/vi/EIlp8wyY97c/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA5tHsd-3VRm6M4O649M3JRMMjAPg",
-        index: 1,
+        index: 2,
         description:
           "I went back to redo the missing color video from last year but this time using oklab and some math to figure them out.",
         id: "EIlp8wyY97c",
-        title: " The Real Missing Colors of Minecraft (*1.20) ",
+        title: "The Real Missing Colors of Minecraft (*1.20)",
       },
       {
         thumbnail:
-          "https://i.ytimg.com/vi/PHDznOL3qzs/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCHp--CW2PNCQEnB53U79dFcqkkZA",
-        index: 2,
-        description: "How to use one noteblock to run multiple commands.",
-        id: "PHDznOL3qzs",
-        title: " Noteblock command tutorial ",
+          "https://i.ytimg.com/vi/khsviAjdjHg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBcaNNax-rGxlVsyjrAe41_jNAoFw",
+        index: 0,
+        description: "I just wanted to check out if the new paintings are actually darker like everyone is saying.",
+        id: "khsviAjdjHg",
+        title: "Are the new 1.21 paintings really that dark?",
       },
     ];
     return latestVideos;
