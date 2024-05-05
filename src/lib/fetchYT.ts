4b86498ca2cfb57f1bf3a3cc64c483bb3b9b2105
Inbestigator <inbestigatoryt@gmail.com>
2024-05-04 22:12:32 -0700

    Update fetchYT.ts

--- src/lib/fetchYT.ts
+++ src/lib/fetchYT.ts
@@ -5,6 +5,15 @@ export async function getLatestVideos(): Promise<Video[]> {
     const latestVideos = [
       {
         thumbnail:
+          "https://i.ytimg.com/vi/khsviAjdjHg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBcaNNax-rGxlVsyjrAe41_jNAoFw",
+        index: 0,
+        description:
+          "I just wanted to check out if the new paintings are actually darker like everyone is saying.",
+        id: "khsviAjdjHg",
+        title: "Are the new 1.21 paintings really that dark?",
+      },
+      {
+        thumbnail:
           "https://i.ytimg.com/vi/6nulqmkLKqI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCY-iVXyn9jnEYFTvH2lPFcXAuADg",
         index: 1,
         description:
@@ -20,14 +29,6 @@ export async function getLatestVideos(): Promise<Video[]> {
           "I went back to redo the missing color video from last year but this time using oklab and some math to figure them out.",
         id: "EIlp8wyY97c",
         title: "The Real Missing Colors of Minecraft (*1.20)",
-      },
-      {
-        thumbnail:
-          "https://i.ytimg.com/vi/khsviAjdjHg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBcaNNax-rGxlVsyjrAe41_jNAoFw",
-        index: 0,
-        description: "I just wanted to check out if the new paintings are actually darker like everyone is saying.",
-        id: "khsviAjdjHg",
-        title: "Are the new 1.21 paintings really that dark?",
       },
     ];
     return latestVideos;
