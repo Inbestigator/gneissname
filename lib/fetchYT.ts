5c1659085eb77bdae533d818f9568ce118354dcc
Inbestigator <inbestigatoryt@gmail.com>
2024-06-01 23:11:58 -0700

    Update fetchYT.ts

--- lib/fetchYT.ts
+++ lib/fetchYT.ts
@@ -5,8 +5,17 @@ export async function getLatestVideos(): Promise<Video[]> {
     const latestVideos = [
       {
         thumbnail:
-          "https://i.ytimg.com/vi/e0HM_vfSuDw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCa7TSWZ8ixd6hEdoroeW92UfsRag",
+          "https://i.ytimg.com/vi/p-TzJihKn1Q/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8vlqOvR256K-lECYFg8aJ5zMkvw",
         index: 0,
+        description:
+          "1.20.6 added yet another format to display colors in Minecraft.",
+        id: "p-TzJihKn1Q",
+        title: "Why does Minecraft keep adding new color formats?",
+      },
+      {
+        thumbnail:
+          "https://i.ytimg.com/vi/e0HM_vfSuDw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCa7TSWZ8ixd6hEdoroeW92UfsRag",
+        index: 1,
         description:
           "Talking about the pitfalls of averaging colors and how gamma and color space affects it.",
         id: "e0HM_vfSuDw",
@@ -15,20 +24,11 @@ export async function getLatestVideos(): Promise<Video[]> {
       {
         thumbnail:
           "https://i.ytimg.com/vi/khsviAjdjHg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBcaNNax-rGxlVsyjrAe41_jNAoFw",
-        index: 1,
+        index: 2,
         description:
           "I just wanted to check out if the new paintings are actually darker like everyone is saying.",
         id: "khsviAjdjHg",
         title: "Are the new 1.21 paintings really that dark?",
-      },
-      {
-        thumbnail:
-          "https://i.ytimg.com/vi/6nulqmkLKqI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCY-iVXyn9jnEYFTvH2lPFcXAuADg",
-        index: 2,
-        description:
-          "Discussion on how geology affects people and what geologists do.",
-        id: "6nulqmkLKqI",
-        title: "Why care about Geology",
       },
     ]
     return latestVideos
