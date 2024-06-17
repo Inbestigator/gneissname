c10ef9982db15b18c12698f8f9856fa79ec1f8f7
Inbestigator <inbestigatoryt@gmail.com>
2024-06-16 21:22:31 -0700

    Update fetchYT.ts

--- lib/fetchYT.ts
+++ lib/fetchYT.ts
@@ -5,8 +5,17 @@ export async function getLatestVideos(): Promise<Video[]> {
     const latestVideos = [
       {
         thumbnail:
-          "https://i.ytimg.com/vi/p-TzJihKn1Q/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8vlqOvR256K-lECYFg8aJ5zMkvw",
+          "https://i.ytimg.com/vi/YbclYBh9n8I/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCKWUgtxeAUG2suznDkTeseZLgsEQ",
         index: 0,
+        description:
+          "Somewhat of a tutorial for how i go about animating the blocks in my videos.",
+        id: "YbclYBh9n8I",
+        title: "Block and block_display animations",
+      },
+      {
+        thumbnail:
+          "https://i.ytimg.com/vi/p-TzJihKn1Q/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8vlqOvR256K-lECYFg8aJ5zMkvw",
+        index: 1,
         description:
           "1.20.6 added yet another format to display colors in Minecraft.",
         id: "p-TzJihKn1Q",
@@ -15,20 +24,11 @@ export async function getLatestVideos(): Promise<Video[]> {
       {
         thumbnail:
           "https://i.ytimg.com/vi/e0HM_vfSuDw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCa7TSWZ8ixd6hEdoroeW92UfsRag",
-        index: 1,
+        index: 2,
         description:
           "Talking about the pitfalls of averaging colors and how gamma and color space affects it.",
         id: "e0HM_vfSuDw",
         title: "How to average color",
-      },
-      {
-        thumbnail:
-          "https://i.ytimg.com/vi/khsviAjdjHg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBcaNNax-rGxlVsyjrAe41_jNAoFw",
-        index: 2,
-        description:
-          "I just wanted to check out if the new paintings are actually darker like everyone is saying.",
-        id: "khsviAjdjHg",
-        title: "Are the new 1.21 paintings really that dark?",
       },
     ]
     return latestVideos
