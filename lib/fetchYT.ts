956f20b3f32afb985c97af1463106a93335f8450
Inbestigator <inbestigatoryt@gmail.com>
2024-06-24 15:56:26 -0700

    Update fetchYT.ts

--- lib/fetchYT.ts
+++ lib/fetchYT.ts
@@ -5,8 +5,17 @@ export async function getLatestVideos(): Promise<Video[]> {
     const latestVideos = [
       {
         thumbnail:
-          "https://i.ytimg.com/vi/YbclYBh9n8I/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCKWUgtxeAUG2suznDkTeseZLgsEQ",
+          "https://i.ytimg.com/vi/UqA_bVS7IV8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBZUfZHmdTMlMuyPuINtbg9XUymGA",
         index: 0,
+        description:
+          "My discord community setup a server to play together on and I wanted to setup a base on it.",
+        id: "UqA_bVS7IV8",
+        title: "The Gneiss Community Server SMP #01",
+      },
+      {
+        thumbnail:
+          "https://i.ytimg.com/vi/YbclYBh9n8I/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCKWUgtxeAUG2suznDkTeseZLgsEQ",
+        index: 1,
         description:
           "Somewhat of a tutorial for how i go about animating the blocks in my videos.",
         id: "YbclYBh9n8I",
@@ -15,20 +24,11 @@ export async function getLatestVideos(): Promise<Video[]> {
       {
         thumbnail:
           "https://i.ytimg.com/vi/p-TzJihKn1Q/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8vlqOvR256K-lECYFg8aJ5zMkvw",
-        index: 1,
+        index: 2,
         description:
           "1.20.6 added yet another format to display colors in Minecraft.",
         id: "p-TzJihKn1Q",
         title: "Why does Minecraft keep adding new color formats?",
-      },
-      {
-        thumbnail:
-          "https://i.ytimg.com/vi/e0HM_vfSuDw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCa7TSWZ8ixd6hEdoroeW92UfsRag",
-        index: 2,
-        description:
-          "Talking about the pitfalls of averaging colors and how gamma and color space affects it.",
-        id: "e0HM_vfSuDw",
-        title: "How to average color",
       },
     ]
     return latestVideos
