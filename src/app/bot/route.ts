1889dc9cb111fdd6120d4069ee4a59af8e7103b3
Inbestigator <inbestigatoryt@gmail.com>
2025-05-15 18:23:42 -0700

    Logs

--- src/app/bot/route.ts
+++ src/app/bot/route.ts
@@ -1,8 +1,9 @@
-// import createHandler from "@dressed/next";
-// import { commands, components, events } from "@/../bot.gen";
+import createHandler from "@dressed/next";
+import { commands, components, events } from "@/../bot.gen";
 
-// export const POST = createHandler(commands, components, events);
-
-export const POST = () => {
-  return new Response("hi");
+export const POST = (r: Request) => {
+  console.log("Here");
+  const handler = createHandler(commands, components, events);
+  console.log("Down here");
+  return handler(r);
 };
