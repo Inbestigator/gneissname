e266a77ddcd413990ab8db97fced4db4b0e7c00d
Inbestigator <inbestigatoryt@gmail.com>
2025-05-15 18:29:36 -0700

    Don't import bot.gen

--- src/app/bot/route.ts
+++ src/app/bot/route.ts
@@ -1,9 +1,9 @@
 import createHandler from "@dressed/next";
-import { commands, components, events } from "@/../bot.gen";
+// import { commands, components, events } from "@/../bot.gen";
 
 export const POST = (r: Request) => {
   console.log("Here");
-  const handler = createHandler(commands, components, events);
+  const handler = createHandler([], [], []);
   console.log("Down here");
   return handler(r);
 };
