26f6230638ae9c5fd6bb9031079b5adab9dcb58b
Inbestigator <inbestigatoryt@gmail.com>
2025-05-15 18:20:13 -0700

    Test res working

--- src/app/bot/route.ts
+++ src/app/bot/route.ts
@@ -1,22 +1,8 @@
-import {
-  handleRequest,
-  setupCommands,
-  setupComponents,
-  setupEvents,
-} from "dressed/server";
-import { commands, components, events } from "@/../bot.gen";
-import { waitUntil } from "@vercel/functions";
+// import createHandler from "@dressed/next";
+// import { commands, components, events } from "@/../bot.gen";
 
-export function POST(req: Request) {
-  const [runCommand, runComponent, runEvent] = [
-    setupCommands(commands),
-    setupComponents(components),
-    setupEvents(events),
-  ];
-  return handleRequest(
-    req,
-    async (i) => waitUntil(runCommand(i)),
-    async (i) => waitUntil(runComponent(i)),
-    async (e) => waitUntil(runEvent(e)),
-  );
-}
+// export const POST = createHandler(commands, components, events);
+
+export const POST = () => {
+  return new Response("hi");
+};
