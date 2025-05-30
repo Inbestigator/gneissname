41b16584b2756ced3141698a8ab691f957dbf9f6
Inbestigator <inbestigatoryt@gmail.com>
2025-05-30 01:57:34 -0700

    Fix action regex

--- src/bot/components/buttons/ticket-action.ts
+++ src/bot/components/buttons/ticket-action.ts
@@ -7,7 +7,7 @@ import {
 } from "dressed";
 import { openTicket } from "../selects/ticket-open";
 
-export const pattern = "ticket-:action(close|open-:ticketname{-:message})";
+export const pattern = "ticket-:action(close|open){-:ticketname{-:message}}";
 
 export default async function ticketButton(
   interaction: MessageComponentInteraction,
