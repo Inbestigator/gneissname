74e67ba4aec7461a9007f334b9536a3d2a95256a
Inbestigator <inbestigatoryt@gmail.com>
2025-05-30 23:29:42 -0700

    Console log

--- src/bot/components/buttons/guess.ts
+++ src/bot/components/buttons/guess.ts
@@ -25,6 +25,7 @@ export default async function guess(
     | { answerId: string; hashed: string }
     | { depAnswerId: string; depHashed: string },
 ) {
+  console.log(args, "answerId" in args);
   const { answerId, hashed } =
     "answerId" in args
       ? args
