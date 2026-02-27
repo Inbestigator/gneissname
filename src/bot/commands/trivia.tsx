1dafcef5530edc9ded773c6461a68664a6baea89
Inbestigator <inbestigatoryt@gmail.com>
2026-02-26 23:15:59 -0800

    feat: sort by timestamp

--- src/bot/commands/trivia.tsx
+++ src/bot/commands/trivia.tsx
@@ -224,7 +224,10 @@ function ResponsesSection({ responses, answerIds }: Readonly<{ responses: Trivia
         />
       }
     >
-      ## {responses.sort((a, b) => Number(b.isCorrect) - Number(a.isCorrect)).map((r) => r.symbol)}
+      ##
+      {responses
+        .sort((a, b) => Number(b.isCorrect) - Number(a.isCorrect) || a.timestamp - b.timestamp)
+        .map((r) => r.symbol)}
       {"⚫".repeat(10 - responses.length)}
     </Section>
   );
