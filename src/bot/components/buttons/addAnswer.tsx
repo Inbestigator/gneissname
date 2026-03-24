ce02b021968f7f6f7d1b404ea89da7b5b221d1e0
Inbestigator <inbestigatoryt@gmail.com>
2026-03-23 23:16:24 -0700

    fix: isTrue

--- src/bot/components/buttons/addAnswer.tsx
+++ src/bot/components/buttons/addAnswer.tsx
@@ -5,18 +5,19 @@ import abseil from "abseil";
 export const pattern = "addAnswer-:isTrue-:questionId";
 
 export default function addAnswer(interaction: MessageComponentInteraction, args: Params<typeof pattern>) {
+  const isTrue = args.isTrue === "true";
   let correct = abseil(interaction.message.components ?? [])
     .initial("TextDisplay") //  Sgt
     .sibling("TextDisplay") //  Qtn
     .sibling("TextDisplay") //  Exp
     .sibling("TextDisplay"); // Cor
-  if (args.isTrue === "false") correct = correct.sibling("TextDisplay").sibling("TextDisplay");
+  if (!isTrue) correct = correct.sibling("TextDisplay").sibling("TextDisplay");
   return interaction.showModal(
     <>
       <Label label="Text">
         <TextInput
           custom_id="text"
-          value={correct.value.content.replace(args.isTrue ? /^Correct: / : /^\d+?\. /, "")}
+          value={correct.value.content.replace(isTrue ? /^Correct: / : /^\d+?\. /, "")}
           required
         />
       </Label>
