c6d198f1bb31fc55780f9a64b3e01eaad805e7be
Inbestigator <inbestigatoryt@gmail.com>
2026-07-25 21:53:38 -0700

    fix: improve leaderboard promising

--- src/bot/commands/leaderboard.tsx
+++ src/bot/commands/leaderboard.tsx
@@ -8,11 +8,7 @@ export const config = {
   description: "View the users with the highest social credit",
 } satisfies CommandConfig;
 
-function Leaderboard({
-  entries,
-}: Readonly<{
-  entries: { credit: number | "‎"; id: string; name: string | Promise<string> }[];
-}>) {
+function Leaderboard({ entries }: Readonly<{ entries: { credit: number | "‎"; id: string; name: Promise<string> }[] }>) {
   return entries.map(({ credit, id, name }, i) => (
     <TextDisplay key={id}>
       {i + 1}. **<Suspense fallback=" ">{name}</Suspense>**
@@ -22,27 +18,31 @@ function Leaderboard({
   ));
 }
 
+const placeholder = (
+  <Leaderboard
+    entries={Array.from({ length: 10 }, (_, i) => ({ credit: "‎", id: `${i}`, name: Promise.resolve(" ") }))}
+  />
+);
+
 export default async function leaderboard(interaction: CommandInteraction) {
   const rankPromise = cache.getRank(interaction.user.id);
   const ranksPromise = cache.getTopUsers();
-  const userPromises = ranksPromise.then((r) =>
-    r.map(({ id }) => cache.getUser(id).then((u) => u.global_name ?? u.username ?? "")),
+  const entriesPromise = ranksPromise.then((rows) =>
+    rows.map((row) => ({ ...row, name: cache.getUser(row.id).then((u) => u.global_name ?? u.username ?? "") })),
   );
-  interaction.reply(
-    <Container>
-      ## Leaderboard{"\n"}
-      Members with the highest social credit You&apos;re #<Suspense>{rankPromise}</Suspense>
-      <Suspense
-        fallback={
-          <Leaderboard entries={Array.from({ length: 10 }, (_, i) => ({ credit: "‎", id: `${i}`, name: " " }))} />
-        }
-      >
-        {ranksPromise.then((r) => (
-          <Leaderboard entries={r.map((u, i) => ({ ...u, name: userPromises.then((p) => p[i]) }))} />
-        ))}
-      </Suspense>
-    </Container>,
-    { ephemeral: true },
+  return procrastinate(
+    entriesPromise,
+    interaction.reply(
+      <Container>
+        ## Leaderboard{"\n"}
+        Members with the highest social credit You&apos;re #<Suspense>{rankPromise}</Suspense>
+        <Suspense fallback={placeholder}>
+          {entriesPromise.then((entries) => (
+            <Leaderboard entries={entries} />
+          ))}
+        </Suspense>
+      </Container>,
+      { ephemeral: true },
+    ),
   );
-  return procrastinate(rankPromise, ...(await userPromises));
 }
