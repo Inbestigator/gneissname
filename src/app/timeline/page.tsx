f7a7e97dc5d8c01cb1329a25919209ab6075155b
Gneiss <156830587+Gneiss-name@users.noreply.github.com>
2026-07-30 10:22:46 -0500

    feat: temp timeline page

---
+++ src/app/timeline/page.tsx
@@ -0,0 +1,15 @@
+export default function TimelineInfoPage() {
+    return (
+        <>
+            <section className="mt-4 mb-8 flex flex-col gap-6" style={{margin: 'auto auto 20px auto'}}>
+                <header className="flex w-full flex-col items-center gap-2 text-center">
+                    <h1 className="font-extrabold text-4xl tracking-tighter sm:text-7xl">Geologic Timeline Project</h1>
+                </header>
+            </section>
+
+            <div style={{textAlign: 'center', fontSize: '1.3rem', margin: 'auto auto 20px auto'}}>
+                It’s not time yet.
+            </div>
+        </>
+    )
+}
