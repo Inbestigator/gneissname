308d31dd689c925a841a38b0d0e93eb7fc173199
Gneiss <156830587+Gneiss-name@users.noreply.github.com>
2026-08-02 15:15:32 -0500

    Update page.tsx

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -2,7 +2,7 @@ export default function TimelineInfoPage() {
   return (
     <div className="mt-6 mb-8 flex flex-col items-center justify-center gap-4 text-center md:my-10">
       <h1 className="font-semibold text-4xl tracking-tight">Geologic Timeline Project</h1>
-      <div className="text-center text-xl">It’s not line time yet.</div>
+      <div className="text-center text-xl">It’s not time yet.</div>
     </div>
   );
 }
